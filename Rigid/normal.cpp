48a96423f6e0ca47a6594ea8cbf284377f62370c
zhengjia <zhengjia@dhcp037-051-052.wireless.buffalo.edu>
2014-11-09 13:35:27 -0500

    normal update

--- Rigid/normal.cpp
+++ Rigid/normal.cpp
@@ -1,22 +1,66 @@
 #include "RigidBody.h"
+#include <assert.h>
+#include <numeric>
+#include <iostream>
 using namespace std;
+
+#include "Vector.h"
+class MinusClass
+{
+	public:
+		Vector2f operator() (Vector2f p1, Vector2f p2) const {return p1-p2;};
+};
+
 
 void RigidBody::bgwall()
 {
 
-	vector<Particle> TmpVector = {*pVector.end(), pVector, *pVector.begin()};
+	
+	vector<Particle> TmpVector (pVector.begin(), pVector.end());
+	vector<Particle>::iterator TmpIter = TmpVector.begin();
+	TmpVector.insert(TmpIter,*(--pVector.end()));
+	TmpVector.push_back(*pVector.begin());
 
-	for(vector<Particle>::iterator iter=TmpVector.begin()+1; iter != TmpVector.end()-1; iter++)
+
+	assert(TmpVector.size()==Num+2);
+
+	vector<Vector2f> PositionVector;
+	for(vector<Particle>::iterator TmpIter=TmpVector.begin(); TmpIter != TmpVector.end(); TmpIter++)
 	{
-		iter_next = iter+1;
-		iter_last = iter-1;
+		PositionVector.push_back(TmpIter->position);
+	}
 
-		Vector2f next_position = iter_next->position;
-		Vector2f last_position = iter_last->position;
 
-		float slope = (next_position.z - last_position.z)/(next_position.x - last_position.x);
+	 vector<Vector2f> NextPos (PositionVector.begin()+1, PositionVector.end());
+	 vector<Vector2f> LastPos (PositionVector.begin(),PositionVector.end()-1);
+
+	assert(NextPos.size()==Num+1);
+	assert(LastPos.size()==Num+1);
+
+	transform(LastPos.begin(), LastPos.end(), NextPos.begin(), LastPos.begin(), MinusClass());
+
+	for(vector<Vector2f>::iterator iter=LastPos.begin(); iter != LastPos.end(); iter++)
+	{
+		if(iter->x==0.0 && iter->z > 0.0)
+		{
+			bg_tang.push_back(Vector2f(0,1.));
+			bg_normal.push_back(Vector2f(-1.0,0));
+		}
+		else if(iter->x==0.0 && iter->z < 0.0)
+		{
+			bg_tang.push_back(Vector2f(0,-1.0));
+			bg_normal.push_back(Vector2f(1.0,0));
+		}
+		else
+		{
+		float slope = (iter->z)/(iter->x);
 		bg_tang.push_back(normalize(Vector2f(1,slope)));
 		bg_normal.push_back(normalize(Vector2f(-slope,1)));
+		}
 	}
+
+	 NextPos.clear();
+	 LastPos.clear();
+	 PositionVector.clear();
 }
 
